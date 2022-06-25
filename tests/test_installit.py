d744af6c59cd14e2971feeb9a25c0bb9625a6813
Hendrik du Toit <hendrik@brightedge.co.za>
2022-06-25 02:00:01 +0200

    GitHub_Issue_00005

--- tests/test_installit.py
+++ tests/test_installit.py
@@ -1,7 +1,7 @@
 '''Testing installit()'''
 
 from pathlib import Path
-from beetools.beearchiver import Archiver
+from beetools import Archiver, beeutils
 import installit
 
 
@@ -19,6 +19,17 @@ class TestInstallIt:
         t_installit = installit.InstallIt()
 
         assert t_installit.success
+        pass
+
+    def test_configure_mysql_remote_access(self, env_setup_self_destruct):
+        env_setup = env_setup_self_destruct
+        t_installit = installit.InstallIt()
+        if t_installit.curr_os == beeutils.LINUX:
+            t_installit.configure_mysql_remote_access(
+                env_setup.mysql_user_admin,
+                env_setup.mysql_user_remote_users,
+                env_setup.mysql_user_remote_rights,
+            )
         pass
 
 
